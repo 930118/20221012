673f879ba927eb13015eefad73da3f278a896e0d
930118 <115605286+930118@users.noreply.github.com>
2022-10-12 12:16:52 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+  stroke(255)
+  noFill();
+  rectMode(CENTER)//以方框為中心點為座標點
+
+  for(var j=0;j<(height/50);j++){
+    for(var i=0;i<(width/50);i=i+1)
+    {
+      ellipse(25+(i*50),25+(j*50),50+mouseX/10)
+      stroke(241,91,181)
+      rect(25+(i*50),25+(j*50),50)
+      stroke(0,245,212)
+      ellipse(50+(i*50),50+(j*50),25+mouseY/5)
+      stroke(255)
+    }
+  }
+}
